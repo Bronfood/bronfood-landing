d84925fb810a47455a834eb0f2c31dbcf8aa9b5e
SergeyGetmanskiy <getmansky1997@yandex.ru>
2026-02-27 16:32:47 +0300

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,23 +8,6 @@ const _inter = Inter({ subsets: ["latin", "cyrillic"] });
 export const metadata: Metadata = {
   title: 'Bronfood — Сервис заказа еды на вынос',
   description: 'Bronfood — цифровая платформа для предварительного заказа еды на вынос (самовывоз) из кофеен и заведений общественного питания.',
-  icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
-  },
 }
 
 export const viewport: Viewport = {
